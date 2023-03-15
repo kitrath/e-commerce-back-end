ad360fa607d9146ee1155ecafc863b2a1eefc482
kitrath <crathmel@gmail.com>
2023-03-15 18:27:33 -0500

    add post, put tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,12 +37,46 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const newTag = await Tag.create(req.body);
+    const hasProductIds = Object.prototype.hasOwnProperty.call(req.body, 'productIds');
+    
+    if (hasProductIds && req.body.productIds.length) {
+      const productTagArr = req.body.productIds.map((product_id) => {
+        return {
+          product_id,
+          tag_id: newTag.id,
+        };
+      });
+
+      const productTags = await ProductTag.bulkCreate(productTagArr);
+
+      res.status(200).json({ tag: newTag, product_tags: productTags })
+    } else {
+      // No productIds, just return new tag
+      res.status(200).json(newTag);
+    }
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tagId = req.params.id;
+    const tagData = await Tag.update(res.body);
+
+    if (!tagData) {
+      res.status(404).json({ message: `No tag found with id ${tagId}` });
+    }
+
+    res.status(200).json(tagData);
+
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
